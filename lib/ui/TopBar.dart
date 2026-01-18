945834f62bd24438c014a420c9cb71ce1b11a47b
Themasterunicalmorta <stefanodragone2812@gmail.com>
2026-01-18 17:39:51 +0100

    Aggiornato Musicservices, aggiunto interno delle cartelle e widget per far scorrere il testo

--- lib/ui/TopBar.dart
+++ lib/ui/TopBar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Topbar extends StatelessWidget {
-
   final double height;
   final double width;
 
@@ -9,25 +8,21 @@ class Topbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            alignment: Alignment.bottomCenter,
-            height: height,
-            width: width,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            padding: EdgeInsets.only(top: height * 0.055),
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        alignment: Alignment.bottomCenter,
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
           ),
+          color: Theme.of(context).colorScheme.primary,
         ),
-      ],
+        padding: EdgeInsets.only(top: height * 0.055),
+      ),
     );
   }
 }
